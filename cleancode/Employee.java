3473f7923d313f782df33e6a2d12acc3afdd8298
Randy070503 <dyran707@gmail.com>
2023-11-08 08:46:23 +0700

    perubahan switch statements

--- cleancode/Employee.java
+++ cleancode/Employee.java
@@ -1,17 +1,25 @@
 package cleancode;
 
-class Employee {
-    ...
-    int payAmount() {
-        switch (getType()) {
-            case EmployeeType.ENGINEER:
-            return _monthlySalary;
-            case EmployeeType.SALESMAN:
-            return _monthlySalary + _commission;
-            case EmployeeType.MANAGER:
-            return _monthlySalary + _bonus;
-            default:
-            throw new Exception("Incorrect Employee");
-        }
+abstract class EmployeeType {
+    abstract int payAmount(EmployeeType emp);
+
+    public int getMonthlySalary() {
+        return 0;
+    }
+    public int getCommission() {
+        return 0;
+    }
+    public int getBonus() {
+        return 0;
+    }
+}
+class Salesman {
+    int payAmount(EmployeeType emp) {
+        return emp.getMonthlySalary() + emp.getCommission();
+    }
+}
+class Manager {
+    int payAmount(EmployeeType emp) {
+        return emp.getMonthlySalary() + emp.getBonus();
     }
 }
