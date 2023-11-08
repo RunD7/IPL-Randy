f6728ab44cac321846ecceebb8ff922be4e0a492
Randy070503 <dyran707@gmail.com>
2023-11-08 09:15:33 +0700

    perubahan formatting

--- cleancode/FitNesseExpediter.java
+++ cleancode/FitNesseExpediter.java
@@ -1,5 +1,9 @@
 package cleancode;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class FitNesseExpediter implements ResponseSender {
     private Socket socket;
     private InputStream input;
@@ -11,5 +15,4 @@ public class FitNesseExpediter implements ResponseSender {
     private long requestProgress;
     private long requestParsingDeadline;
     private boolean hasError;
-    ...
 }
