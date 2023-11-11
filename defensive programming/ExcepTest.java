32ebd455af64561d7f94bf346e0275c93253aeb9
Randy070503 <dyran707@gmail.com>
2023-11-11 19:14:24 +0700

    the finally block

--- defensive programming/ExcepTest.java
+++ defensive programming/ExcepTest.java
@@ -1,31 +1,14 @@
-import java.io.*;
-
 public class ExcepTest {
     public static void main(String args[]) {
-        FileInputStream file = null;  // Mendeklarasikan objek FileInputStream
-        byte x;
-        // Menambahkan blok try-catch untuk menangani IOException
+        int a[] = new int[2];
         try {
-            String fileName = "example.txt"; // Gantilah dengan nama file yang sesuai
-            file = new FileInputStream(fileName);
-            x = (byte) file.read();
-        } catch (FileNotFoundException f) {
-            f.printStackTrace();
-            return; // Menghentikan program jika file tidak ditemukan
-        } catch (IOException i) {
-            i.printStackTrace();
-            return; // Menghentikan program jika terjadi IOException
-        } finally {
-            // Menutup file dalam blok finally agar selalu dieksekusi
-            if (file != null) {
-                try {
-                    file.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            System.out.println("Access element three :" + a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown :" + e);
+        }finally {
+            a[0] = 6;
+            System.out.println("First element value: " + a[0]);
+            System.out.println("The finally statement is executed");
         }
-        // Melanjutkan eksekusi program setelah blok try-catch
-        System.out.println("File read successfully!");
     }
 }
