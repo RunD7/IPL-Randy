9509acc5be5a508e8ed0f55fe2d5fdf4e7ab3f47
Randy070503 <dyran707@gmail.com>
2023-11-11 19:01:37 +0700

    multiple catch blocks

--- defensive programming/ExcepTest.java
+++ defensive programming/ExcepTest.java
@@ -2,12 +2,30 @@ import java.io.*;
 
 public class ExcepTest {
     public static void main(String args[]) {
+        FileInputStream file = null;  // Mendeklarasikan objek FileInputStream
+        byte x;
+        // Menambahkan blok try-catch untuk menangani IOException
         try {
-            int a[] = new int[2];
-            System.out.println("Access element three :" + a[3]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Exception thrown :" + e);
+            String fileName = "example.txt"; // Gantilah dengan nama file yang sesuai
+            file = new FileInputStream(fileName);
+            x = (byte) file.read();
+        } catch (FileNotFoundException f) {
+            f.printStackTrace();
+            return; // Menghentikan program jika file tidak ditemukan
+        } catch (IOException i) {
+            i.printStackTrace();
+            return; // Menghentikan program jika terjadi IOException
+        } finally {
+            // Menutup file dalam blok finally agar selalu dieksekusi
+            if (file != null) {
+                try {
+                    file.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        System.out.println("Out of the block");
+        // Melanjutkan eksekusi program setelah blok try-catch
+        System.out.println("File read successfully!");
     }
 }
