4b86f2ac97ef5aa91301f354acb706a6fd09456f
Randy070503 <dyran707@gmail.com>
2023-11-29 08:37:44 +0700

    database

---
+++ Projek/Database/dbtoko.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2023 at 02:05 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbtoko`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `IDCompany` int(5) NOT NULL,
+  `Name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `IDCustomer` int(5) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderr`
+--
+
+CREATE TABLE `orderr` (
+  `IDOrder` int(5) NOT NULL,
+  `IDCustomer` int(5) NOT NULL,
+  `IDProduct` int(5) NOT NULL,
+  `Date` date NOT NULL,
+  `Quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `IDProduct` int(5) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Price` int(8) NOT NULL,
+  `IDCompany` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`IDCompany`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`IDCustomer`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `orderr`
+--
+ALTER TABLE `orderr`
+  ADD PRIMARY KEY (`IDOrder`),
+  ADD KEY `Customer` (`IDCustomer`),
+  ADD KEY `Product` (`IDProduct`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`IDProduct`),
+  ADD KEY `Company` (`IDCompany`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderr`
+--
+ALTER TABLE `orderr`
+  ADD CONSTRAINT `Customer` FOREIGN KEY (`IDCustomer`) REFERENCES `customer` (`IDCustomer`),
+  ADD CONSTRAINT `Product` FOREIGN KEY (`IDProduct`) REFERENCES `product` (`IDProduct`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `Company` FOREIGN KEY (`IDCompany`) REFERENCES `company` (`IDCompany`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
